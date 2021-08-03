6ea8b37f24c3723fc272c63e6c7e58c50c381af4
CitaSiadari03 <80254733+CitaSIadari03@users.noreply.github.com>
2021-08-03 13:17:12 +0700

    Submission Dicoding

---
+++ README.md
@@ -0,0 +1,4 @@
+# dicoding submission
+Hallo Dicoding,<br>
+Ini Submission Kelas <b>"Belajar Dasar Pemrograman Web"</b> Dicoding<br>
+Terimakasih <b>Tetap Jaga Kesehatan</b> Terimakasih Dicoding.<br>
---
+++ assets/main.js
@@ -0,0 +1,8 @@
+console.log("JavaScript berhasil dIsimpan");
+
+//INI PROFIL CITA YA
+
+document.getElementById("awal").innerHTML = "Hallo Semuanya!!!";
+document.getElementById('nama').innerHTML = 'Cita Sari Marito Siadari';
+document.getElementById('date').innerHTML = '03 September 1999';
+document.getElementById('prodi').innerHTML = 'Teknik Informatika';
